f521c61e28e9f386222e19ae0e8a50b5bb56c0a5
nordbyandreas <nordbyandreas@hotmail.com>
2017-08-16 13:07:07 +0200

    uploading stormy project

---
+++ app/src/main/java/com/andnord/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,60 @@
+package com.andnord.stormy.ui;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.andnord.stormy.R;
+import com.andnord.stormy.adapters.DayAdapter;
+import com.andnord.stormy.weather.Day;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DailyForecastActivity extends Activity {
+
+    private Day[] mDays;
+
+    @BindView(android.R.id.list) ListView mListView;
+    @BindView(android.R.id.empty) TextView mEmptyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = (mDays[position].getTemperatureMax()-32)*5/9 + "";
+                String message = String.format("On %s the high will be %s degrees and it will be %s", dayOfTheWeek, highTemp, conditions);
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/andnord/stormy/weather/Current.java
@@ -0,0 +1,92 @@
+package com.andnord.stormy.weather;
+
+import com.andnord.stormy.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Andreas on 01.05.2017.
+ */
+
+public class Current {
+
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId(){
+       return Forecast.getIconId(mIcon);
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public int getTemperature() {
+        return (int) Math.round((mTemperature-32)*5/9);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int)Math.round(mPrecipChance);
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
